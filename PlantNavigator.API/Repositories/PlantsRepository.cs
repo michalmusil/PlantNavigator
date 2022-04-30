2f3a6c305371c7294eac418a1985fed0a9aa85ea
Michal Musil <musilmichal16@gmail.com>
2022-04-30 23:56:58 +0200

    First request from database using EF, Repository and AutoMapper working

--- PlantNavigator.API/Repositories/PlantsRepository.cs
+++ PlantNavigator.API/Repositories/PlantsRepository.cs
@@ -1,5 +1,7 @@
-﻿using PlantNavigator.API.DbContexts;
+﻿using Microsoft.EntityFrameworkCore;
+using PlantNavigator.API.DbContexts;
 using PlantNavigator.API.Entities;
+using System.Linq.Expressions;
 
 namespace PlantNavigator.API.Repositories
 {
@@ -9,6 +11,19 @@ namespace PlantNavigator.API.Repositories
         {
         }
 
+        public override async Task<IEnumerable<Plant>> GetAll(Expression<Func<Plant, bool>> predicate = null)
+        {
+            var all = dbContext.Set<Plant>().AsNoTracking();
+
+            if (predicate != null)
+            {
+                all = all.Where(predicate);
+            }
+
+            all = all.Include(p => p.WaterCondition).Include(p => p.LightCondition);
+
+            return await all.ToListAsync();
+        }
 
     }
 }
